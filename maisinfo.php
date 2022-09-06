399645f6055f3a9404ec6b44e14e079cdbd5acc3
cbperuzzo <caetanob_peruzzo@yahoo.com.br>
2022-09-06 17:11:20 -0300

    tela de login ,css, início

--- maisinfo.php
+++ maisinfo.php
@@ -31,7 +31,7 @@
             <div class="container-fluid">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                        <a class="nav-link active" aria-current="page" href="prin.html">Home</a>
                     </li>
                 </ul>
             </div>
